9b1ad8c76a792eb21e0e89b44f427b1584695c60
Matheus1700 <melomatheus835@gmail.com>
2024-11-29 14:01:49 -0300

    testes e funcionalidades adicionadas

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express');
+const connectDB = require('./db');
+const FilmeController = require('./controllers/FilmeController');
+
 const app = express();
 const PORT = 8080;
 
+connectDB();
 app.use(express.json());
 
+// Rotas
 app.get('/', (req, res) => {
     res.send('Bem-vindo à API de Produtos (Filmes)! Use /api/filmes para acessar os produtos.');
 });
 
-app.get('/api/filmes', (req, res) => {
-    res.json(produtos);
-});
+app.get('/api/filmes', FilmeController.getFilmes);
 
+app.post('/api/filmes', FilmeController.addFilme);
 
+app.delete('/api/filmes/:id', FilmeController.deleteFilme);
+
+// Inicia o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
